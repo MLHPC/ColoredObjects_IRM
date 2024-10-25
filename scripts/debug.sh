9217631c4164fb054fd11aa3bb307131aa207ba7
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-10-25 13:20:01 +0900

    add

---
+++ scripts/debug.sh
@@ -0,0 +1,18 @@
+SHELL_ARGS="--l2_regularizer_weight=0.001 \
+            --lr 0.01 --batch_size=1000 \
+            --penalty_anneal_iter=80 \
+            --opt=sgd \
+            --print_every=10 \
+            --penalty_weight=10000 \
+            --steps=800  \
+            --step_gamma=0.1 \
+            --dataset=ColoredObject
+
+            "
+            
+CMD="qsub -g tge-24IJ0078 run.sh ${SHELL_ARGS}"
+echo "Exp-$((count + 1)): ${CMD}"
+eval $CMD
+
+count=$((count += 1))
+        
