10c38aa4fdf28ce26a6cdf7ee2f29e9eac24b0cd
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-10-25 15:46:19 +0900

    Update debug.sh

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,16 +1,16 @@
 SHELL_ARGS="--l2_regularizer_weight=0.001 \
             --lr 0.005 \
-            --batch_size=1000 \
+            --batch_size=512 \
             --penalty_anneal_iter=80 \
             --opt=sgd \
             --print_every=10 \
             --penalty_weight=10000 \
-            --steps=800  \
+            --steps=1000  \
             --step_gamma=0.1 \
             --dataset=ColoredObject \
-            --irm_type=irmv1_vrex \
+            --irm_type=irmv1_mmrex \
             --var_beta=0. \
-            --min_alpha=-0.2 \
+            --min_alpha=-1 \
             "
             
 CMD="qsub -g tge-24IJ0078 run.sh ${SHELL_ARGS}"
