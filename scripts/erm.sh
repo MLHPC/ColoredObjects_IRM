f2fbf7a60390c09d40a4db368e0e8ca30219351f
Kotaro Yoshida <yoshida.k.bl@m.titech.ac.jp>
2024-10-26 17:03:59 +0900

    add

---
+++ scripts/erm.sh
@@ -0,0 +1,23 @@
+SHELL_ARGS="--l2_regularizer_weight=0.001 \
+            --lr 0.01 \
+            --batch_size=512 \
+            --penalty_anneal_iter=80 \
+            --opt=sgd \
+            --print_every=1 \
+            --penalty_weight=10000 \
+            --steps=1000  \
+            --step_gamma=0.1 \
+            --dataset=ColoredObject \
+            --irm_type=erm \
+            --var_beta=0. \
+            --min_alpha=-1. \
+            --wandb_project_name=ColoredObject_erm \
+            --wandb_entity_name=katoro13 \
+            "
+            
+CMD="qsub -g tge-24IJ0078 run.sh ${SHELL_ARGS}"
+echo "Exp-$((count + 1)): ${CMD}"
+eval $CMD
+
+count=$((count += 1))
+        
---
+++ scripts/irmv1_mmrex.sh
@@ -0,0 +1,27 @@
+MIN_ALPHA_LIST=(-1e-1, -2e-1, -3e-1, -4e-1, -5e-1, -6e-1, -7e-1, -8e-1, -9e-1, -1)
+
+for min_alpha in "${MIN_ALPHA_LIST[@]}" ; do
+
+    SHELL_ARGS="--l2_regularizer_weight=0.001 \
+                --lr 0.01 \
+                --batch_size=512 \
+                --penalty_anneal_iter=80 \
+                --opt=sgd \
+                --print_every=1 \
+                --penalty_weight=10000 \
+                --steps=1000  \
+                --step_gamma=0.1 \
+                --dataset=ColoredObject \
+                --irm_type=irmv1_vrex \
+                --var_beta=0 \
+                --min_alpha=${min_alpha}$ \
+                --wandb_project_name=ColoredObject_irmv1_mmrex \
+                --wandb_entity_name=katoro13 \
+                "
+                
+    CMD="qsub -g tge-24IJ0078 run.sh ${SHELL_ARGS}"
+    echo "Exp-$((count + 1)): ${CMD}"
+    eval $CMD
+
+    count=$((count += 1))
+        
